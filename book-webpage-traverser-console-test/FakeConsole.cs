9dd116cee9112ac785406ebb3d2589e0ae00751c
Mullu <bek200@gmail.com>
2024-05-05 23:28:37 +0200

    Add testcases

---
+++ book-webpage-traverser-console-test/FakeConsole.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace BookWebPageScraper.Tests
+{
+    public static class FakeConsole
+    {
+        private static StringBuilder _output = new StringBuilder();
+        private static TextWriter _originalOutput = Console.Out;
+
+        public static string Output => _output.ToString();
+
+        public static void StartCapture()
+        {
+            _output.Clear();
+            Console.SetOut(new StringWriter(_output));
+        }
+
+        public static void StopCapture()
+        {
+            Console.SetOut(_originalOutput);
+        }
+    }
+}
---
+++ book-webpage-traverser-console-test/ScraperRunnerTests.cs
@@ -0,0 +1,52 @@
+using BookWebPageScraper.src.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace BookWebPageScraper.Tests
+{
+    public class ScraperRunnerTests
+    {
+        [Fact]
+        public async Task ScrapeBookPages_Run_Success()
+        {
+            // Arrange
+            var mockScraperHandler = new Mock<IScraperHandlerService>();
+            mockScraperHandler.Setup(x => x.ScrapeBookPages(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+
+            var serviceProvider = new ServiceCollection()
+                .AddTransient<IScraperHandlerService>(_ => mockScraperHandler.Object)
+                .BuildServiceProvider();
+
+            var scraperRunner = new ScraperRunner(serviceProvider.GetService<IScraperHandlerService>());
+
+            // Act
+            await scraperRunner.Run();
+
+            // Assert
+            Assert.DoesNotContain("Test exception", FakeConsole.Output);
+            mockScraperHandler.Verify(x => x.ScrapeBookPages(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+
+
+        [Fact]
+        public async Task ScrapeBookPages_Run_Exception()
+        {
+            // Arrange
+            FakeConsole.StartCapture();
+            var exceptionMessage = "Test exception";
+            var scraperHandlerMock = new Mock<IScraperHandlerService>();
+            scraperHandlerMock.Setup(x => x.ScrapeBookPages(It.IsAny<string>(), It.IsAny<string>()))
+                              .Throws(new Exception(exceptionMessage));
+
+            var scraperRunner = new ScraperRunner(scraperHandlerMock.Object);
+
+            // Act
+            await scraperRunner.Run();
+            FakeConsole.StopCapture();
+
+            // Assert
+            Assert.Contains(exceptionMessage, FakeConsole.Output);
+        }
+    }
+}
